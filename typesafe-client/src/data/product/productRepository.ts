c9acb8b011d665625d7a71ddc295162a63a6e2e2
eatnug <wlq4568@gmail.com>
2023-06-16 01:26:22 +0900

    add zod

--- typesafe-client/src/data/product/productRepository.ts
+++ typesafe-client/src/data/product/productRepository.ts
@@ -5,7 +5,10 @@ function useProduct(id: number) {
     const [product, setProduct] = useState<Product>()
 
     useEffect(() => {
-        productApi.getProduct(id).then(product => setProduct(product))
+        productApi
+            .getProduct(id)
+            .then(product => setProduct(product))
+            .catch(error => { alert(`오류가 발생했습니다. ${error}`) })
     }, [id])
 
     return product
