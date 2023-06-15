5dd0feee2957f685b5276896a52cb2e4f387dce6
eatnug <wlq4568@gmail.com>
2023-06-16 00:56:10 +0900

    when shipping fee is string

--- typesafe-server/src/data/productRepository.ts
+++ typesafe-server/src/data/productRepository.ts
@@ -1,7 +1,7 @@
 import { PRODUCTS } from "./mockDB";
 
 function getProduct(id: number) {
-    return PRODUCTS.find(user => user.id == id)
+    return { ...PRODUCTS.find(user => user.id == id), shippingFee: '1000' }
 }
 
 export default {
